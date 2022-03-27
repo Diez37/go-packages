df35188d2801134486dac984bfad38d3cb04a2d9
diez37 <diezinthead@gmail.com>
2022-03-28 01:50:53 +0300

    parse ip middleware

---
+++ router/middlewares/ip.go
@@ -0,0 +1,59 @@
+package middlewares
+
+import (
+	"context"
+	"net"
+	"net/http"
+	"strings"
+)
+
+const (
+	IpParamNameDefault = "ip"
+
+	headerNameRealIp       = "X-REAL-IP"
+	headerNameForwardedFor = "X-FORWARDED-FOR"
+)
+
+type IpOption func(ip *IP) *IP
+
+type IP struct {
+	name string
+}
+
+func NewIP(options ...IpOption) Middleware {
+	middleware := &IP{}
+
+	options = append([]IpOption{IpWithName(IpParamNameDefault)}, options...)
+
+	for _, option := range options {
+		middleware = option(middleware)
+	}
+
+	return middleware
+}
+
+func (middleware *IP) Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		address := request.Header.Get(headerNameRealIp)
+		if strings.TrimSpace(address) == "" {
+			address = request.Header.Get(headerNameForwardedFor)
+		}
+		if strings.TrimSpace(address) == "" {
+			address = request.RemoteAddr
+		}
+
+		next.ServeHTTP(
+			writer,
+			request.WithContext(
+				context.WithValue(request.Context(), middleware.name, net.ParseIP(address)),
+			))
+	})
+}
+
+func IpWithName(name string) IpOption {
+	return func(ip *IP) *IP {
+		ip.name = name
+
+		return ip
+	}
+}
